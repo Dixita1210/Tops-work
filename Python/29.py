c001182e633e7b63c53950e3615690f1a23784b4
Dixita1210 <dixitakhimani20@gmail.com>
2025-11-01 10:37:49 +0530

    Update and rename 29 Assignment.py to 29.py

--- Python/29.py
+++ Python/29.py
@@ -1,3 +1,6 @@
+29) Write a Python function to get the largest number, smallest num 
+and sum of all from a list. 
+
 lst_num=[3,67,89,236,56]
 largest = max(lst_num)
 smallest = min(lst_num)
@@ -5,3 +8,4 @@ total = sum(lst_num)
 print(largest)
 print(smallest)
 print(total)
+
