de031835d419b925631d93dc2c17a30d468c114d
Dixita1210 <dixitakhimani20@gmail.com>
2025-11-01 10:26:58 +0530

    Update and rename 11 Assignment.py to 11.py

--- Python/11.py
+++ Python/11.py
@@ -1,6 +1,10 @@
+11) Write a Python program to test whether a passed letter is a vowel 
+or not. 
+
 user_input=input('Enter a letter-')
 vowels=('a','e','i','o','u')
 if user_input in vowels:
     print(f'{user_input} is a vowel' )
 else:
     print(f'{user_input} is not a vowel ')
+
