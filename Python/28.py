d0ef42af57d56d8a1368a172dec0d5872efb4af8
Dixita1210 <dixitakhimani20@gmail.com>
2025-11-01 10:37:18 +0530

    Update and rename 28 Assignment.py to 28.py

--- Python/28.py
+++ Python/28.py
@@ -1,3 +1,5 @@
+28) Differentiate between append () and extend () methods? 
+
 #append will add elements in the end of the list. It will add whole list as one element in previous list 
 
 lst1=[1,2,3,4,5]
@@ -11,3 +13,4 @@ lst1=[1,2,3,4,5]
 lst2=[6,7,8,9,10]
 lst1.extend(lst2)
 print(lst1)
+
