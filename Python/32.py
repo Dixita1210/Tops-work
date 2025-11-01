de8f1f276360f3b1a54bb7a17ea054e0db5a6e9f
Dixita1210 <dixitakhimani20@gmail.com>
2025-11-01 10:39:24 +0530

    Update and rename 32 Assignment.py to 32.py

--- Python/32.py
+++ Python/32.py
@@ -1,6 +1,10 @@
+32) Write a Python program to remove duplicates from a list. 
+
+
 lst=[1,1,2,2,3,3,4,4,5,5,6,6,7,7]
 lst_new=[]
 for i in lst:
     if i not in lst_new:
         lst_new.append(i)
+
 print(lst_new)
