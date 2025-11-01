42a835bdd32ea6e7061b9bd3dc39c13f4966ff7c
Dixita1210 <dixitakhimani20@gmail.com>
2025-11-01 10:31:36 +0530

    Update and rename 18 Assignment.py to 18.py

--- Python/18.py
+++ Python/18.py
@@ -1,7 +1,10 @@
+18) Write a Python program to count occurrences of a substring in a string. 
+
 str=input('Enter a string-')
 substr=input('Enter a substring to be count-')
 count=str.count(substr)
 print(f'The substring {substr} occurs {count} times in string')
 #print(count)
 #print(str.count(substr))
+
 
