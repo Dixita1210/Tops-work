4275a0d82d1eaa67a3c3af01e71da8776c79e148
Dixita1210 <dixitakhimani20@gmail.com>
2025-11-01 10:32:08 +0530

    Update and rename 19 Assignment.py to 19.py

--- Python/19.py
+++ Python/19.py
@@ -1,6 +1,9 @@
+19) Write a Python program to count the occurrences of each word in a  
+
 str='Hello Good Good Morning Hello '
 dict_freq={}
 word=str.split() #here split will give output in a list so we have to iterate tge word variable with i
 for i in word:
     dict_freq[i]=str.count(i)
 print(dict_freq)
+
