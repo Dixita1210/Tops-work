5f05e8a61d766671b0a9022df55043664bb96666
Dixita1210 <dixitakhimani20@gmail.com>
2025-11-01 10:27:39 +0530

    Update and rename 12 Assignment.py to 12.py

--- Python/12.py
+++ Python/12.py
@@ -1,3 +1,6 @@
+12) Write a Python program to sum of three given integers. However, if 
+two values are equal sum will be zero. 
+
 num1=int(input('Enter number 1-'))
 num2=int(input('Enter number 2-'))
 num3=int(input('Enter number 3-'))
@@ -6,3 +9,4 @@ print(sum)
 if num1==num2 or num1==num3 or num2==num3:
     sum=0
     print(sum)
+
