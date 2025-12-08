69ae451c82c1d066e5279e47161edb171d46dada
Dixita1210 <dixitakhimani20@gmail.com>
2025-12-08 20:18:03 +0530

    Update 05.py

--- Python/05.py
+++ Python/05.py
@@ -1,9 +1,10 @@
-5) Write a Python program to get the Factorial number of given numbers.
+#5) Write a Python program to get the Factorial number of given numbers.
 
     num=int(input('Enter a number-'))
 fac=1    #here fac is a variable where the factorial will be stored. It can not be zero because the i will  be multiplied with 0 and answer will be 0 
 for i in range(1,num+1):
     fac=fac*i
 print('Factorial of given number is',fac)
+
 
 
