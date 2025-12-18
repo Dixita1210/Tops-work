59f421a22bb671526e87341a383352c779f08e69
Dixita1210 <dixitakhimani20@gmail.com>
2025-12-18 20:09:02 +0530

    Update 45.py

--- Python/45.py
+++ Python/45.py
@@ -1,9 +1,10 @@
 # Write a Python program to unzip a list of tuples into individual lists. 
 
 data = [(1, 2), (3, 4), (5, 6)]
-list1, list2 = zip(*data)
+list1, list2 = zip(*data)  #zip will pair 1st elemets together and 2nd elements together thats why we have two list here 
 
 print("List 1:", list(list1))
 print("List 2:", list(list2))
+
 
    
