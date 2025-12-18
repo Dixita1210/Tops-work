f89d74de96fac43c2541c8e15ba36f533e70dde9
Dixita1210 <dixitakhimani20@gmail.com>
2025-12-18 19:58:59 +0530

    Update 34.py

--- Python/34.py
+++ Python/34.py
@@ -1,10 +1,9 @@
 #Write a Python function that takes two lists and returns true if they have at least one common member.
 
-lst1=[1,2,3,4,5,6,7,8]
-lst2=[1,1,2,3,4,5,6]
-for i in lst1:
-    if i in lst2:
-        print('True')
-        break
-else:
-  print('False')
+def common_member(list1, list2):
+    for item in list1:         
+        if item in list2:       
+            return True         
+    return False                
+print(common_member([1, 2, 3], [4, 5, 3]))   
+print(common_member(['a', 'b'], ['x', 'y']))
