1e0473ce4b58a18ac9c4a98d10dd4504a4ddf0f7
Dixita1210 <dixitakhimani20@gmail.com>
2025-12-18 20:46:33 +0530

    Update 80.py

--- Python/80.py
+++ Python/80.py
@@ -1,12 +1,14 @@
 # Write a Python program to count the frequency of words in a file. 
+from collections import Counter
+
+
 with open("AI.txt", "r") as file:
-    text = file.read()  
-words = text.split()  #splitting words from sentence
-word_count = {}       #create new dict to store word and count
-for i in words:
-    if  i in  word_count:
-        word_count[i] += 1
-    else:
-        word_count[i] = 1
-for i, count in word_count.items():
-    print(f"{i}: {count}")
+    text = file.read()
+
+words = text.split()
+word_count = Counter(words)
+
+for word, count in word_count.items():
+    print(f"{word}: {count}")
+
+
