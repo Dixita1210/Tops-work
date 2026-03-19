21a1b275adfd8e13ae141e69275b3a8fb850876d
Dixita1210 <dixitakhimani20@gmail.com>
2026-03-19 18:23:48 +0530

    Add files via upload

---
+++ Libraries/Libraries Assessment.py
@@ -0,0 +1,25 @@
+# 1) Import pandas and read in the banklist.csv file into a dataframe called banks.
+
+import pandas as pd
+banks=pd.read_csv("C:\\Users\\sanja\\OneDrive\\Desktop\\Banklist.csv")
+# print(banks)
+
+# 2) Show the head of the dataframe.
+
+# print(banks.head())
+
+# What are the column names?
+
+# print(banks.info())
+
+# How many States (ST) are represented in this data set?
+
+# print(len(banks["ST"].unique()))
+
+# Get a list or array of all the states in the data set.
+
+# print(banks["ST"].unique())
+
+
+
+
