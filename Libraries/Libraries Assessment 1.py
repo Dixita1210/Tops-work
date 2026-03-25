cb65b40e917b91a04ceeaf7dca5bee98d4b7d04b
Dixita1210 <dixitakhimani20@gmail.com>
2026-03-25 13:05:28 +0530

    Update Libraries Assessment 1.py

--- Libraries/Libraries Assessment 1.py
+++ Libraries/Libraries Assessment 1.py
@@ -14,11 +14,23 @@ print(banks.info())
 
 # 4) How many States (ST) are represented in this data set?
 
- print(len(banks["ST"].unique()))
+ print(banks["ST"].nunique())
 
 # 5) Get a list or array of all the states in the data set.
 
  print(banks["ST"].unique())
+
+# 6) Top 5 states with most failed banks
+
+ print(banks["ST"].value_counts().head(5))
+
+# 7) Top 5 acquiring institutions
+
+print(banks["Acquiring Institution"].value_counts().head(5))
+
+# 8) State Bank of Texas acquisitions
+
+print(((banks['Acquiring Institution'] == 'State Bank of Texas') & (banks['ST'] == 'TX')).sum())
 
 
 
